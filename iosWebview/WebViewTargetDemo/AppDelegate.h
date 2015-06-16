490b381a8d10399880fb3494f7ccc72fa370c53a
tingbaozhao <zhaotingbao@sina.cn>
2015-06-16 23:52:18 +0800

    ios webView project

---
+++ iosWebview/WebViewTargetDemo/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  WebViewTargetDemo
+//
+//  Created by  tingbao.zhao on 15/5/23.
+//  Copyright (c) 2015年 Appium Committers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ iosWebview/WebViewTargetDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WebViewTargetDemo
+//
+//  Created by  tingbao.zhao on 15/5/23.
+//  Copyright (c) 2015年 Appium Committers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.helloWorldLabel.
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
